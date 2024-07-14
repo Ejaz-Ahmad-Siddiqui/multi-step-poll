d8bd5551e80984d378b0759e25cb3a8c08c884a1
Ejaz Ahmad Siddiqui <iamejaazahmad@gmail.com>
2024-07-14 16:55:39 +0500

    final commit

--- src/components/__tests__/Option.test.tsx
+++ src/components/__tests__/Option.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Option from '../Option';
 
 const mockOption = { id: "1", label: "Good", icon: "👍" };
